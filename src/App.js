d84f849024a1a443a202ae0da5ca1452d482e9c1
/SabrinaRey <sabrinareynares@gmail.com>
2020-04-08 21:43:54 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+
+
+  const [pais, setPais] = useState([]);
+  const [busqueda, setBusqueda] = useState('Argentina')
+
+  const buscarPais = () => {
+    fetch(`https://restcountries.eu/rest/v2/name/${busqueda}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setPais(data)
+      });
+  }
+
+  useEffect(() => {
+    buscarPais()
+  }, [busqueda]);
+
+
+  const handleChange = e => {
+    setBusqueda(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="card">
+
+        {pais.length >= 0 && pais.map(element => {
+          return (<>
+            <img alt={element.name} src={element.flag}></img>
+            <p> <span>Nombre:</span> {element.name}</p>
+            <p><span>Capital:</span> {element.capital}</p>
+            <p><span>Población:</span> {element.population}</p>
+          </>
+          )
+        }
+        )}
+        <form onSubmit={handleSubmit}>
+          <input value={busqueda} onChange={handleChange} />
+          <input className= "buttonBuscar" type="submit" value="Buscar País" />
+        </form>
+      </div>
     </div>
   );
 }
